5330fbf37bb93b6eb0e1afdf7a10eeeaef3c4842
Arsen Hryhoriak <136361705+Lifuss@users.noreply.github.com>
2024-01-12 20:19:32 +0200

    tailwind+clsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import StatusLabels, { Status } from './components/SatusLabels';
+
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
+      <h1 className="text-2xl">Home page</h1>
+      <StatusLabels status={Status.Active}>Active</StatusLabels>
+      <StatusLabels status={Status.NotActive}>Not Active</StatusLabels>
+      <StatusLabels status={Status.Pending}>Pending</StatusLabels>
+      <StatusLabels status={Status.Suspended}>Suspended</StatusLabels>
     </main>
   );
 }
