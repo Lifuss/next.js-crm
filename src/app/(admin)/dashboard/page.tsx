0e17dfcd658c97f391d61fd2cb30e4f6658b13b6
Arsen Hryhoriak <136361705+Lifuss@users.noreply.github.com>
2024-01-27 21:50:36 +0200

    clear magicButton

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import MagicButton from '@/app/components/magicButton';
 import React from 'react';
 
 type PageProps = {};
@@ -7,7 +6,6 @@ const Page = (props: PageProps) => {
   return (
     <main>
       <h1 className="text-xl">Dashboard</h1>
-      <MagicButton />
     </main>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import AddCompanyButton from './components/AddCompanyButton';
 import StatusLabels, { Status } from './components/StatusLabels';
 import { cookies } from 'next/headers';
-import MagicButton from './components/magicButton';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-2xl">Home page {new Date().toTimeString()}</h1>
       <AddCompanyButton />
-      <MagicButton />
     </main>
   );
 }
