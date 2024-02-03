ccf08ceb54a395fcae4d9c3fd0179de5983b648c
Arsen Hryhoriak <arsen.hryh@gmail.com>
2024-02-03 18:17:42 +0200

    api fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,10 @@
 export interface SummaryStats {
-  promotions: string;
-  categories: string;
+  promotions: number;
+  categories: number;
   newCompanies: number;
   activeCompanies: number;
 }
+
 export interface SummarySales {
   id: string;
   companyId: string;
@@ -12,22 +13,12 @@ export interface SummarySales {
   income: number;
 }
 
-export interface Promotions {
+export interface Country {
   id: string;
   title: string;
-  description: string;
-  discount: number;
-  companyId: string;
-  companyTitle: string;
-  avatar?: string;
 }
 
 export interface Category {
-  id: string;
-  title: string;
-}
-
-export interface Country {
   id: string;
   title: string;
 }
@@ -53,6 +44,16 @@ export interface Company {
   avatar?: string;
 }
 
+export interface Promotion {
+  id: string;
+  title: string;
+  description: string;
+  discount: number;
+  companyId: string;
+  companyTitle: string;
+  avatar?: string;
+}
+
 const PROJECT_TOKEN = process.env.NEXT_PUBLIC_PROJECT_TOKEN;
 
 const buildUrl = (...paths: string[]) =>
@@ -61,10 +62,9 @@ const buildUrl = (...paths: string[]) =>
 const stringifyQueryParams = (params: Record<string, string>) =>
   new URLSearchParams(params).toString();
 
-const SendRequest = async <T>(url: string, init?: RequestInit) => {
+const sendRequest = async <T>(url: string, init?: RequestInit) => {
   const res = await fetch(url, init);
   if (!res.ok) {
-    console.log(res);
     throw new Error(await res.text());
   }
 
@@ -72,34 +72,64 @@ const SendRequest = async <T>(url: string, init?: RequestInit) => {
 };
 
 export const getSummaryStats = (init?: RequestInit) => {
-  return SendRequest<SummaryStats>(buildUrl('summary-stats', '1'), init);
+  return sendRequest<SummaryStats>(buildUrl('summary-stats', '1'), init);
 };
 
 export const getSummarySales = (init?: RequestInit) => {
-  return SendRequest<SummarySales[]>(buildUrl('summary-sales'), init);
+  return sendRequest<SummarySales[]>(buildUrl('summary-sales'), init);
 };
 
 export const getCountries = (init?: RequestInit) => {
-  return SendRequest<Country[]>(buildUrl('countries'), init);
+  return sendRequest<Country[]>(buildUrl('countries'), init);
 };
 
 export const getCategories = (init?: RequestInit) => {
-  return SendRequest<Category[]>(buildUrl('categories'), init);
+  return sendRequest<Category[]>(buildUrl('categories'), init);
 };
 
 export const getCompanies = (init?: RequestInit) => {
-  return SendRequest<Company[]>(buildUrl('companies'), init);
+  return sendRequest<Company[]>(buildUrl('companies'), init);
 };
 
 export const getCompany = (id: string, init?: RequestInit) => {
-  return SendRequest<Company>(buildUrl('companies', id), init);
+  return sendRequest<Company>(buildUrl('companies', id), init);
 };
 
 export const getPromotions = async (
   params: Record<string, string> = {},
   init?: RequestInit,
 ) => {
-  return SendRequest<Promotions[]>(
+  return sendRequest<Promotion[]>(
     `${buildUrl('promotions')}?${stringifyQueryParams(params)}`,
+    init,
   );
+};
+
+export const createCompany = async (
+  data: Omit<Company, 'id' | 'hasPromotions'>,
+  init?: RequestInit,
+) => {
+  return sendRequest<Company>(buildUrl('companies'), {
+    ...init,
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      ...(init && init.headers),
+      'content-type': 'application/json',
+    },
+  });
+};
+
+export const createPromotion = async (
+  data: Omit<Promotion, 'id'>,
+  init?: RequestInit,
+) => {
+  return sendRequest<Promotion>(buildUrl('promotions'), {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      ...(init && init.headers),
+      'content-type': 'application/json',
+    },
+  });
 };
