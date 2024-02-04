266cee33653932e0e3f29226ce57198182075a61
Arsen Hryhoriak <arsen.hryh@gmail.com>
2024-02-04 20:50:49 +0200

    Revert "api fix"
    This reverts commit ccf08ceb54a395fcae4d9c3fd0179de5983b648c.

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,9 @@
 export interface SummaryStats {
-  promotions: number;
-  categories: number;
+  promotions: string;
+  categories: string;
   newCompanies: number;
   activeCompanies: number;
 }
-
 export interface SummarySales {
   id: string;
   companyId: string;
@@ -13,12 +12,22 @@ export interface SummarySales {
   income: number;
 }
 
-export interface Country {
+export interface Promotions {
+  id: string;
+  title: string;
+  description: string;
+  discount: number;
+  companyId: string;
+  companyTitle: string;
+  avatar?: string;
+}
+
+export interface Category {
   id: string;
   title: string;
 }
 
-export interface Category {
+export interface Country {
   id: string;
   title: string;
 }
@@ -44,16 +53,6 @@ export interface Company {
   avatar?: string;
 }
 
-export interface Promotion {
-  id: string;
-  title: string;
-  description: string;
-  discount: number;
-  companyId: string;
-  companyTitle: string;
-  avatar?: string;
-}
-
 const PROJECT_TOKEN = process.env.NEXT_PUBLIC_PROJECT_TOKEN;
 
 const buildUrl = (...paths: string[]) =>
@@ -62,9 +61,10 @@ const buildUrl = (...paths: string[]) =>
 const stringifyQueryParams = (params: Record<string, string>) =>
   new URLSearchParams(params).toString();
 
-const sendRequest = async <T>(url: string, init?: RequestInit) => {
+const SendRequest = async <T>(url: string, init?: RequestInit) => {
   const res = await fetch(url, init);
   if (!res.ok) {
+    console.log(res);
     throw new Error(await res.text());
   }
 
@@ -72,64 +72,34 @@ const sendRequest = async <T>(url: string, init?: RequestInit) => {
 };
 
 export const getSummaryStats = (init?: RequestInit) => {
-  return sendRequest<SummaryStats>(buildUrl('summary-stats', '1'), init);
+  return SendRequest<SummaryStats>(buildUrl('summary-stats', '1'), init);
 };
 
 export const getSummarySales = (init?: RequestInit) => {
-  return sendRequest<SummarySales[]>(buildUrl('summary-sales'), init);
+  return SendRequest<SummarySales[]>(buildUrl('summary-sales'), init);
 };
 
 export const getCountries = (init?: RequestInit) => {
-  return sendRequest<Country[]>(buildUrl('countries'), init);
+  return SendRequest<Country[]>(buildUrl('countries'), init);
 };
 
 export const getCategories = (init?: RequestInit) => {
-  return sendRequest<Category[]>(buildUrl('categories'), init);
+  return SendRequest<Category[]>(buildUrl('categories'), init);
 };
 
 export const getCompanies = (init?: RequestInit) => {
-  return sendRequest<Company[]>(buildUrl('companies'), init);
+  return SendRequest<Company[]>(buildUrl('companies'), init);
 };
 
 export const getCompany = (id: string, init?: RequestInit) => {
-  return sendRequest<Company>(buildUrl('companies', id), init);
+  return SendRequest<Company>(buildUrl('companies', id), init);
 };
 
 export const getPromotions = async (
   params: Record<string, string> = {},
   init?: RequestInit,
 ) => {
-  return sendRequest<Promotion[]>(
+  return SendRequest<Promotions[]>(
     `${buildUrl('promotions')}?${stringifyQueryParams(params)}`,
-    init,
   );
-};
-
-export const createCompany = async (
-  data: Omit<Company, 'id' | 'hasPromotions'>,
-  init?: RequestInit,
-) => {
-  return sendRequest<Company>(buildUrl('companies'), {
-    ...init,
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      ...(init && init.headers),
-      'content-type': 'application/json',
-    },
-  });
-};
-
-export const createPromotion = async (
-  data: Omit<Promotion, 'id'>,
-  init?: RequestInit,
-) => {
-  return sendRequest<Promotion>(buildUrl('promotions'), {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      ...(init && init.headers),
-      'content-type': 'application/json',
-    },
-  });
 };
